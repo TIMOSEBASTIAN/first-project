42cbea14c7b2bee42e23317bdc05528583af4058
unknown <sebastianaashidle03@gmail.com>
2023-07-11 18:07:00 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+function changeColor() {
+  document.querySelector(".fargeBTN").style.color = "#589962";
+}
+function changeColor2() {
+  document.querySelector(".fargeBTN").style.color = "#EB984E";
+}
+function changeColor3() {
+  document.querySelector(".fargeBTN").style.color = "#7020A5";
+}
+function changeColor4() {
+  document.querySelector(".fargeBTN").style.color = "#F0F3F4";
+}
